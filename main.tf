658028e3fad13984b4aab15da06388b13971ca79
jzjudith <jzjudith@github.com>
2022-10-11 21:43:33 +0100

    update

--- main.tf
+++ main.tf
@@ -7,7 +7,10 @@ module "azurermcompute" {
   virtual_network_name = var.virtual_network_name
   vnet_cidr_space      = var.vnet_cidr_space
 
+  subnets = { private_subnet = ["10.0.1.0/24"] }
+
   ip_config_name = var.ip_config_name
+  compute_count  = 1
 
   tags = var.tags
 
