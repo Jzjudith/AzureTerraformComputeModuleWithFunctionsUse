7060e78cefd6378b3a031580cffdbd84f4f28e8e
jzjudith <jzjudith@github.com>
2022-10-14 16:53:52 +0100

    newoutput

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "public_ip_address" {
   description = "The public ip address"
-  value       = module.azurermcompute.public_ip_address
+  value       = "${module.azurermcompute.public_ip_address}"
 }
 
 output "public_ip_address_id" {
   description = "The id of the public ip address"
-  value       = module.azurermcompute.public_ip_address_id
+  value       = "${module.azurermcompute.public_ip_address_id}"
 }
 
 output "subnet_ids" {
   description = "The ids of the subnet"
-  value       = module.azurermcompute.subnet_ids
+  value       = "${module.azurermcompute.subnet_ids}"
 }
 
 output "network_interface_ids" {
   description = "The id of the network interface"
-  value       = module.azurermcompute.network_interface_ids
+  value       = "${module.azurermcompute.network_interface_ids}"
 }
 
